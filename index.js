fc7e010b7d6f115d87b9e2926d09fac2f4012047
Joshua Garcia <josshuagarciia000@gmail.com>
2023-03-12 10:00:26 -0700

    Added grid.

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+const createCard = (src, span, title, desc) => {
+
+    return `
+    <img class="object-cover" src="assets/img/download.webp" alt="" srcset="">
+    <span>${span}</span>
+    <h2>${title}</h2>
+    <hr>
+    <p>${desc}</p>`;
+}
